4c485e04c3428ac6a747442c9907fbcba733e80c
carnasci <cen.master24@gmail.com>
2025-01-31 17:26:19 -0600

    Day 31 - setup front of flashcard UI

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+right_button_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_img, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+wrong_button_img = PhotoImage(file="images/wrong.png")
+wrong_button_button = Button(image=wrong_button_img, highlightthickness=0)
+wrong_button_button.grid(row=1, column=0)
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
