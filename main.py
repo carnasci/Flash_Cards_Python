a249c35ba5e3f40ba68e47ca7b26e581b9302208
carnasci <cen.master24@gmail.com>
2025-02-05 16:50:57 -0600

    Day 31 - trying to get card to flip after 5 seconds

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-
+current_card = random.choice(to_learn)
 
 def next_word():
-    current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=current_card["French"])
 
-
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
 #------------------- FRONT CARD UI ---------------------#
@@ -26,7 +28,8 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
