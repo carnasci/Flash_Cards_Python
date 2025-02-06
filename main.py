10f359cfc47b4b98f8d6d1f2ed3bca2f3ca18a03
carnasci <cen.master24@gmail.com>
2025-02-06 16:49:15 -0600

    Day 31 - added a way to remove already known words from the list and the ability to save the list of not known words for use the next time the program runs

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
 #----------------------- Logic -------------------------#
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card = {}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_word():
     global current_card, flip_timer
@@ -24,6 +31,11 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
+def right_button_click():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_word()
 
 #------------------- FRONT CARD UI ---------------------#
 
@@ -42,7 +54,7 @@ card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold")
 canvas.grid(row=0, column=0, columnspan=2)
 
 right_button_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_img, highlightthickness=0, command=next_word)
+right_button = Button(image=right_button_img, highlightthickness=0, command=right_button_click)
 right_button.grid(row=1, column=1)
 
 wrong_button_img = PhotoImage(file="images/wrong.png")
