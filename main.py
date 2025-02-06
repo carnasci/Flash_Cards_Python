d354cd6c3ef58b45b03a0f9e67bea00c1fdb09ad
carnasci <cen.master24@gmail.com>
2025-02-06 16:18:24 -0600

    Day 31 - fixed 5 second timer bug

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-current_card = random.choice(to_learn)
+current_card = {}
 
 def next_word():
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    flip_timer = window.after(5000, func=flip_card)
 
 def flip_card():
     canvas.itemconfig(canvas_image, image=card_back_img)
@@ -26,6 +31,8 @@ window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = window.after(5000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
@@ -33,8 +40,6 @@ canvas_image = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
-
-window.after(5000, flip_card)
 
 right_button_img = PhotoImage(file="images/right.png")
 right_button = Button(image=right_button_img, highlightthickness=0, command=next_word)
